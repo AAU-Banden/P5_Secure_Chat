b9ac7a93a10921d5e4ca4001c3f4e7cb8043124d
Anton <akroghp@gmail.com>
2019-10-22 14:32:43 +0200

    Initial commit

---
+++ app/src/main/java/aau/itcom/group_2/p5_secure_chatting/MainActivity.java
@@ -0,0 +1,13 @@
+package aau.itcom.group_2.p5_secure_chatting;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
